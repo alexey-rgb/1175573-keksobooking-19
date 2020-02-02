91cb1523d08879fd36f4fa975a931566ca76028d
Алексей <askerbl67@gmail.com>
2020-02-02 23:25:28 +0300

    11:25 правки

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@
 var OBJECT_NUMBER = 8;
 var MIN = 1;
 var MAX = 10;
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var MAX_PRICE = 5000;
+var MIN_PRICE = 1000;
+var TYPES = ['Дворец', 'Квартира', 'Дом', 'Бунгало'];
 var CHECKS = ['12:00', '13:00', '14:00'];
-var MAX_MAP_WIDTH = 3500;
-var MIN_MAP_WIDTH = 1;
+var MAX_MAP_WIDTH = 100;
+var MIN_MAP_WIDTH = 20;
 var POSITION_Y_MIN = 130;
 var POSITION_Y_MAX = 630;
 var PIN_SIZE = 40;
@@ -18,7 +20,7 @@ var PHOTO_APARTMENTS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'h
 
 var CARD_TEMPLATE = document.querySelector('#card').content.querySelector('.map__card');
 var PIN_TEMPLATE = document.querySelector('#pin').content.querySelector('.map__pin');
-var PIN_WRAPPER = document.querySelector('.map__pins');
+/*var PIN_WRAPPER = document.querySelector('.map__pins');*/
 var MAP = document.querySelector('.map');
 
 // создаем функции, для получения прозвольных значений свойств объекта(описание объявления)
@@ -51,15 +53,15 @@ var mockData = function () {
       'avatar': 'img/avatars/user0' + (i + 1) + '.png',
       'title': 'заголовок объявления',
       'address': '{' + String(positionX()) + ',' + String(positionY()) + '}',
-      'price': random(MIN, MAX),
+      'price': random(MIN_PRICE, MAX_PRICE) + ' Рублей/ночь',
       'type': getRandomElementArray(TYPES),
       'rooms': random(MIN, MAX),
       'guests': random(MIN, MAX),
       'checkin': getRandomElementArray(CHECKS),
       'checkout': getRandomElementArray(CHECKS),
       'features': 'wifi, dishwasher, parking, washer, elevator, conditioner',
-      'description': 'строка с описанием',
-      'photos': getRandomElementArray(PHOTO_APARTMENTS),
+      'description': 'Уютное местечко',
+      'photos': PHOTO_APARTMENTS,
       'location': {
         'x': positionX(),
         'y': positionY(),
@@ -77,14 +79,14 @@ MAP.classList.remove('map--faded');
 
 var getNewDescription = function (item) {
   var cardElement = CARD_TEMPLATE.cloneNode(true);
-  cardElement.querySelector('.popup__photo').src = item.photos;
+  cardElement.querySelector('.popup__photo').src = item.photos[0];
   cardElement.querySelector('.popup__avatar').src = item.avatar;
   cardElement.querySelector('.popup__title').textContent = item.title;
   cardElement.querySelector('.popup__text--address').textContent = item.address;
   cardElement.querySelector('.popup__text--price').textContent = item.price;
   cardElement.querySelector('.popup__type').textContent = item.type;
-  cardElement.querySelector('.popup__text--capacity').textContent = item.rooms + item.guests;
-  cardElement.querySelector('.popup__text--time').textContent = item.checkin + item.checkout;
+  cardElement.querySelector('.popup__text--capacity').textContent = item.rooms + ' комнаты для ' + item.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + item.checkin + ', выезд до ' + item.checkout;
   cardElement.querySelector('.popup__features').textContent = item.features;
   cardElement.querySelector('.popup__description').textContent = item.description;
   return cardElement;
@@ -94,25 +96,27 @@ var getNewPin = function (item) {
   var pinElement = PIN_TEMPLATE.cloneNode(true);
   pinElement.querySelector('.pin__img').src = item.avatar;
   pinElement.querySelector('.pin__img').alt = item.title;
-  pinElement.style.left = (item.location.x + (-PIN_SIZE * 0.5)) + 'px';
+  pinElement.style.left = (item.location.x + (-PIN_SIZE * 0.5)) + '%';
   pinElement.style.top = (item.location.y + (-PIN_SIZE * 0.5)) + 'px';
   return pinElement;
 };
 
-// записываем полученную информацию во фрагмент
+// записываем во фрагмент
 
 var fragment1 = document.createDocumentFragment();
-mockData().forEach(function (item, i) {
+mockData().forEach(function (item) {
   fragment1.appendChild(getNewPin(item));
 });
 
 // выводим фрагмент в dom
 
-PIN_WRAPPER.appendChild(fragment1);
+MAP.appendChild(fragment1);
+
+// записываем во фрагмент
 
 var fragment = document.createDocumentFragment();
 fragment.appendChild(getNewDescription(mockData()[0]));
 
 // выводим фрагмент в dom
 
-PIN_WRAPPER.appendChild(fragment);
+MAP.appendChild(fragment);
