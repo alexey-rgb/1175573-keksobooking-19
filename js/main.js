a699e5942ee40258d3ca2f29e77ed7a7d8cbc66d
Алексей <askerbl67@gmail.com>
2020-02-17 19:01:56 +0300

    задание 4.2 и 4.3

--- js/main.js
+++ js/main.js
@@ -23,12 +23,7 @@ var MapWidth = {
   MAX: Math.floor(MAX_MAP_PERCENT_WIDTH - PIN_PERCENT_SIZE),
   MIN: Math.floor(PIN_PERCENT_SIZE)
 };
-
 var CHECKS = ['12:00', '13:00', '14:00'];
-var Position = {
-  Y_MIN: 130,
-  Y_MAX: 630,
-};
 var TYPES = ['Дворец', 'Квартира', 'Дом', 'Бунгало'];
 var PHOTO_APARTMENTS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
@@ -37,45 +32,35 @@ var PHOTO_APARTMENTS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
 var PIN_POSITION_FORMULA = (PIN_PERCENT_SIZE * 0.5);
 var APARTMENTS_ADVANTAGES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
-// объявляем константы для доступа к dom-элементам
+// Перечисление
+
+var Position = {
+  Y_MIN: 130,
+  Y_MAX: 630,
+};
+
+// перечисление для доступа к dom-элементам
 
 var Nodes = {
   CARD_TEMPLATE: document.querySelector('#card').content.querySelector('.map__card'),
   PIN_TEMPLATE: document.querySelector('#pin').content.querySelector('.map__pin'),
   MAP: document.querySelector('.map'),
-  form: document.querySelector('.ad-form'),
-  mainPin: document.querySelector('.map__pin--main'),
+  FORM: document.querySelector('.ad-form'),
+  MAIN_PIN: document.querySelector('.map__pin--main'),
 
-  // переменные, касаящиеся главного пина(маффин).
-
-  inputAddress: document.querySelector('#address'),
+  // касаящиеся главного пина(маффин).
+  INPUT_ADDRESS: document.querySelector('#address'),
+  // касается полей форм
+  BUTTON_SUBMIT: document.querySelector('.ad-form__submit'),
+  TYPE: document.querySelector('#type'),
+  PRICE_INPUT: document.querySelector('#price'),
+  TIME_IN: document.querySelector('#timein'),
+  TIME_OUT: document.querySelector('#timeout'),
+  FIELDSET: document.querySelectorAll('.ad-form__element'),
+  POPUP_PHOTO: document.querySelector('.popup__photo')
 };
 
-// координаты гланого пина
-
-var mainPinPosition = (MainPinPosition.X + (PIN_PIXEL_SIZE) * 0.5) + ', ' + (MainPinPosition.Y + MAIN_PIN_HEIGHT + PIN_PSEUDO);
-
-// переменная содержит метод добавления координат в поле адрес
-
-var addressAttribute = Nodes.inputAddress.setAttribute('value', mainPinPosition + '');
-
-// функция активации страницы
-
-var pageActivation = function () {
-  // активируем форму
-  Nodes.form.classList.remove('ad-form--disabled');
-
-  // активируем поля ввода
-  fieldset.forEach(function (item) {
-    item.removeAttribute('disabled');
-  });
-
-  // выводим фрагмент c метками похожих объявлений в dom
-  Nodes.MAP.appendChild(fillFragment(process));
-
-  // показываем карту обявлений
-  Nodes.MAP.classList.remove('map--faded');
-};
+// Объект для управления полями ввода(синхронизация)
 
 var RoomData = {
   palace: {
@@ -100,6 +85,126 @@ var RoomData = {
   }
 };
 
+// координаты гланого пина
+
+var mainPinPosition = (MainPinPosition.X + (PIN_PIXEL_SIZE) * 0.5) + ', ' + (MainPinPosition.Y + MAIN_PIN_HEIGHT + PIN_PSEUDO);
+
+// переменная содержит метод добавления координат в поле адрес
+
+var addressAttribute = Nodes.INPUT_ADDRESS.setAttribute('value', mainPinPosition + '');
+
+// функция отрисовки координат метки в поле ввода адреса.
+
+var fixAddressValue = function () {
+  return addressAttribute;
+};
+
+// синхронизация полей ввода гостей и количества комнат
+
+var findDifference = function () {
+  var roomNumber = document.querySelector('#room_number').value;
+  var capacity = document.querySelector('#capacity');
+  var inputGuestsChangeNumberHandler = function () {
+    return capacity.value;
+  };
+  capacity.addEventListener('change', inputGuestsChangeNumberHandler);
+  if (roomNumber !== capacity.value) {
+    capacity.setCustomValidity('кол-во гостей должны быть равно количеству комнат');
+  } else {
+    capacity.setCustomValidity('');
+  }
+};
+
+// ПОЧЕМУ НЕ ПОЛУЧАЕТСЯ ЭТУ ФУНКЦИЮ В РАЗМЕТКУ ONCHAGE ЗАПИСАТЬ. ХОЧЕТСЯ БЕЗ СОБЫТИЯ НА BUTTON СРАБОТАЛО?
+
+var validateForm = function () {
+  var titleInput = document.querySelector('#title');
+  var MIN_LENGTH = 30;
+  var MAX_LENGTH = 30;
+  titleInput.addEventListener('change', function () {
+    if (titleInput.value.trim().length < MIN_LENGTH && titleInput.value.trim().length < MAX_LENGTH) {
+      titleInput.setCustomValidity('не меньше 30 и не больше 100 символов');
+    } else {
+      titleInput.setCustomValidity('');
+    }
+  });
+};
+
+// функция изменения значения поля цены, в зависимости от типа жилья
+
+var inputApartChangeTypeHandler = function (evt) {
+  if (evt.target === Nodes.TYPE) {
+    Nodes.PRICE_INPUT.placeholder = RoomData[Nodes.TYPE.value].price;
+    Nodes.PRICE_INPUT.min = RoomData[Nodes.TYPE.value].price;
+  }
+};
+
+// функция изменения занчения полей времени заезда/выезда
+
+var inputTimeChangeValue = function () {
+  if (Nodes.TIME_IN.value === '12:00') {
+    Nodes.TIME_OUT.value = Nodes.TIME_IN.value;
+  }
+  if (Nodes.TIME_IN.value === '13:00') {
+    Nodes.TIME_OUT.value = Nodes.TIME_IN.value;
+  }
+  if (Nodes.TIME_IN.value === '14:00') {
+    Nodes.TIME_OUT.value = Nodes.TIME_IN.value;
+  }
+};
+
+// общая функция активации страницы
+
+var pageActivation = function () {
+  // активируем форму
+  Nodes.FORM.classList.remove('ad-form--disabled');
+  // активируем поля ввода
+  Nodes.FIELDSET.forEach(function (item) {
+    item.removeAttribute('disabled');
+  });
+  // выводим фрагмент c метками похожих объявлений в dom
+  Nodes.MAP.appendChild(fillFragment(process));
+  // показываем карту обявлений
+  Nodes.MAP.classList.remove('map--faded');
+  // отслеживаем изменения и по необходимости изменяем подсказки в полях
+  Nodes.BUTTON_SUBMIT.addEventListener('click', findDifference);
+  Nodes.BUTTON_SUBMIT.addEventListener('click', validateForm);
+  // 1 способ синхронизации двух полей. рабочий
+  Nodes.TYPE.addEventListener('change', inputApartChangeTypeHandler);
+  // изменяется время выезда гостей при изменение времени заезда
+  Nodes.TIME_IN.addEventListener('change', inputTimeChangeValue);
+  // блокируем возможность редактирования поля с адресом
+
+  Nodes.INPUT_ADDRESS.setAttribute('disabled', 'disabled');
+};
+
+// функция активации страницы при клике на главный пин и на ENTER
+
+var mainPinClickHandler = function (evt) {
+  if (typeof evt === 'object') {
+    switch (evt.button) {
+      case 0:
+        break;
+    }
+    pageActivation();
+    fixAddressValue();
+    Nodes.POPUP_PHOTO.hidden = true;
+  }
+};
+
+
+var mainPinKeyDownHandler = function (evt) {
+  if (evt.key === 'Enter') {
+    pageActivation();
+  }
+};
+
+var activatePageWorking = function () {
+  // активация страницы кликом
+  Nodes.MAIN_PIN.addEventListener('mousedown', mainPinClickHandler);
+  // активация страницы с клавиатуры
+  Nodes.MAIN_PIN.addEventListener('keydown', mainPinKeyDownHandler);
+};
 
 // создаем функции, для получения прозвольных значений свойств объекта(описание объявления)
 
@@ -134,7 +239,7 @@ var getRandomItems = function (arr) {
 };*/
 
 // функция которая внтури цикла длинною arr.length записывает сгенерированные <img> во фрагмент
-// и на каждом витке добавляет в src + class
+// и на каждом витке добавляет src + class
 
 var makeImage = function (arr) {
   var count = getRandomBetween(1, PHOTO_APARTMENTS.length);
@@ -177,7 +282,7 @@ var mockData = function () {
   return objects;
 };
 
-// записываем функцию получения массива с объектами рандомных свойств в переменную
+// записываем в переменную функцию получения массива с объектами рандомных свойств
 
 var process = mockData();
 
@@ -185,10 +290,10 @@ var process = mockData();
 
 var getNewDescription = function (item) {
   var cardElement = Nodes.CARD_TEMPLATE.cloneNode(true);
-  var removeCardElement = function () {
+  var buttonCloseHandler = function () {
     cardElement.remove();
   };
-  var removeCardElementEsc = function (evt) {
+  var buttonCloseOnEscHandler = function (evt) {
     if (evt.key === 'Escape') {
       cardElement.remove();
     }
@@ -205,18 +310,20 @@ var getNewDescription = function (item) {
   cardElement.querySelector('.popup__photos').appendChild(item.photos);
   cardElement.querySelector('.popup__photo').remove();
   cardElement.querySelector('.popup__close').setAttribute('tabindex', '1');
-  cardElement.querySelector('.popup__close').addEventListener('click', removeCardElement);
-  document.addEventListener('keydown', removeCardElementEsc);
+  cardElement.querySelector('.popup__close').addEventListener('click', buttonCloseHandler);
+  document.addEventListener('keydown', buttonCloseOnEscHandler);
   return cardElement;
 };
-
 
 // функция создания пина
 
 var getNewPin = function (item) {
   // создаем клон карточки используя произвольный объект для заполнения шаблона
   var mock = getNewDescription(item);
-
+  var pinClickHandler = function () {
+    // выводим мок в dom
+    Nodes.MAP.appendChild(mock);
+  };
   // клонируем
   var pinElement = Nodes.PIN_TEMPLATE.cloneNode(true);
   pinElement.querySelector('img').src = item.avatar;
@@ -224,13 +331,8 @@ var getNewPin = function (item) {
   pinElement.style.left = (item.location.x) + (-PIN_POSITION_FORMULA) + '%';
   pinElement.style.top = 'calc(' + (item.location.y) + 'px + ' + (-PIN_POSITION_FORMULA * 2) + '%)';
   pinElement.setAttribute('tabindex', '1');
-
   // при клике на пин показываем карточку с описанием
-  pinElement.addEventListener('click', function () {
-
-    // выводим мок в dom
-    Nodes.MAP.appendChild(mock);
-  });
+  pinElement.addEventListener('click', pinClickHandler);
   return pinElement;
 };
 
@@ -246,63 +348,11 @@ var fillFragment = function () {
 
 // блокируем поля ввода
 
-var fieldset = Nodes.form.querySelectorAll('.ad-form__element');
-fieldset.forEach(function (item) {
+Nodes.FIELDSET.forEach(function (item) {
   item.setAttribute('disabled', 'disabled');
 });
 
-// блокируем возможность редактирования поля с адресом
-
-Nodes.inputAddress.setAttribute('disabled', 'disabled');
-
-// функция отрисовки координат метки в поле ввода адреса.
-
-var fixAddressValue = function () {
-  return addressAttribute;
-};
-
-// активация страницы кликом
-
-Nodes.mainPin.addEventListener('mousedown', function (evt) {
-    if (typeof evt === 'object') {
-      switch (evt.button) {
-        case 0:
-          break;
-      }
-      pageActivation();
-      fixAddressValue();
-      document.querySelector('.popup__photo').hidden = true;
-    }
-  }
-);
-
-// активация страницы с клавиатуры
-
-Nodes.mainPin.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Enter') {
-      pageActivation();
-    }
-  }
-);
-
-// синхронизация полей ввода гостей и количества комнат
-
-var findDifference = function () {
-  var roomNumber = document.querySelector('#room_number').value;
-  var capacity = document.querySelector('#capacity');
-  capacity.addEventListener('change', function () {
-    return capacity.value;
-  });
-  if (roomNumber !== capacity.value) {
-    capacity.setCustomValidity('кол-во гостей должны быть равно количеству комнат');
-  } else {
-    capacity.setCustomValidity('');
-  }
-};
-
-var buttonSubmit = document.querySelector('.ad-form__submit');
-
-buttonSubmit.addEventListener('click', findDifference);
+activatePageWorking();
 
 /* вариант синхронизации полей(гости и комнаты) через добавления атрибута onchange тегу #capacity разметки
 
@@ -326,38 +376,10 @@ nodes.MAP.addEventListener('click', function (evt) {
 });
 */
 
-// ПОЧЕМУ НЕ ПОЛУЧАЕТСЯ ЭТУ ФУНКЦИЮ В РАЗМЕТКУ ONCHAGE ЗАПИСАТЬ. ХОЧЕТСЯ БЕЗ СОБЫТИЯ НА BUTTON СРАБОТАЛО?
-
-var validateForm = function () {
-  var titleInput = document.querySelector('#title');
-  var MIN_LENGTH = 30;
-  var MAX_LENGTH = 30;
-  titleInput.addEventListener('change', function () {
-    if (titleInput.value.trim().length < MIN_LENGTH && titleInput.value.trim().length < MAX_LENGTH) {
-      titleInput.setCustomValidity('не меньше 30 и не больше 100 символов');
-    } else {
-      titleInput.setCustomValidity('');
-    }
-  });
-};
-
-buttonSubmit.addEventListener('click', validateForm);
-
-// 1 способ синхронизации двух полей. рабочий
-
-/*var priceInput = document.querySelector('#price');
-var type = document.querySelector('#type');
-type.addEventListener('change', function (evt) {
-  if (evt.target === type) {
-    priceInput.placeholder = roomData[type.value].price;
-    priceInput.min = roomData[type.value].price;
-  }
-});*/
-
-// 2 способ синхронизации двух полей. КАК ИСПРАВИТЬ ЧТО БЫ ЗАРАБОТАЛО?
+// 2 способ синхронизации двух полей.
 
 // priceInput это поле выбора цены за жилье
-var priceInput = document.querySelector('#price');
+/* var priceInput = document.querySelector('#price');
 (function (element) {
   // type это поле выбора типа жилья
   var type = document.querySelector('#type');
@@ -383,8 +405,9 @@ var priceInput = document.querySelector('#price');
     }
   });
 })(priceInput);
+*/
 
-// 3 способ, РАБОЧИЙ
+// 3 способ синхронизации полей
 
 /*
 var type = document.querySelector('#type');
@@ -409,17 +432,3 @@ type.addEventListener('change', function () {
   }
 });
 */
-
-var timeIn = document.querySelector('#timein');
-var timeOut = document.querySelector('#timeout');
-timeIn.addEventListener('change', function () {
-  if (timeIn.value === '12:00') {
-    timeOut.value = timeIn.value;
-  }
-  if (timeIn.value === '13:00') {
-    timeOut.value = timeIn.value;
-  }
-  if (timeIn.value === '14:00') {
-    timeOut.value = timeIn.value;
-  }
-});
